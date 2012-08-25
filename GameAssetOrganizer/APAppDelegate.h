473acdd1e27489bbae383270d94802b69a1824fb
Benedikt Terhechte <terhechte@gmail.com>
2012-08-25 12:42:17 +0200

    Initial Commit

---
+++ GameAssetOrganizer/APAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APAppDelegate.h
+//  GameAssetOrganizer
+//
+//  Created by BTerhechte on 25.08.12.
+//  Copyright (c) 2012 BTerhechte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface APAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GameAssetOrganizer/APAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  APAppDelegate.m
+//  GameAssetOrganizer
+//
+//  Created by BTerhechte on 25.08.12.
+//  Copyright (c) 2012 BTerhechte. All rights reserved.
+//
+
+#import "APAppDelegate.h"
+
+@implementation APAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
