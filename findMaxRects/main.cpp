dd29d0966a32f2494195bb6649118ea87b56a137
Benedikt Terhechte <terhechte@gmail.com>
2012-08-27 13:22:12 +0200

    added files for upcoming c++ command line target to calculate the size of a sheet in the preview.

---
+++ findMaxRects/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  findMaxRects
+//
+//  Created by BTerhechte on 27.08.12.
+//  Copyright (c) 2012 BTerhechte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // get all 
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
